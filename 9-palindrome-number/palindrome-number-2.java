3c456653277f974ba547f5ab57b806788fac345b
ayush-2986 <2986.ayush@gmail.com>
2025-09-14 19:43:57 +0530

    Create palindrome-number-2.java

---
+++ 9-palindrome-number/palindrome-number-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int a) {
+        if(a<0){
+            return false;
+        }
+        String aStr = String.valueOf(a);
+
+        for(int i=0; i<aStr.length()/2; i++){
+            if(aStr.charAt(i) != aStr.charAt(aStr.length()-1-i)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
