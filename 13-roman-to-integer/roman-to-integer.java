733b3de67f8f8dcea0dd89eac1bd42e38dfc7c42
ayush-2986 <2986.ayush@gmail.com>
2025-10-07 09:40:20 +0530

    Time: 4 ms (70.24%) | Memory: 44.8 MB (70.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,38 +1,16 @@
 class Solution {
     public int romanToInt(String s) {
-        int previous = 0;
-        int sum = 0;
-        int digit;
-        char current_char;
+        int i = s.length()-1;
+        int last = 0;
+        int num = 0;
+        while (i>=0){
+            char letter = s.toUpperCase().charAt(i);
+            int letterValue = (letter == 'I')?1:(letter=='V')?5:(letter=='X')?10:(letter=='L')?50:(letter=='C')?100:(letter=='D')?500:(letter=='M')?1000:0;
 
-        int I = 1;
-        int V = 5;
-        int X = 10;
-        int L = 50;
-        int C = 100;
-        int D = 500;
-        int M = 1000;
-        
-
-        for (int i = s.length()-1; i >= 0; i--){
-            current_char = s.charAt(i);
-            digit = (current_char=='I')?1:
-                    (current_char=='V')?5:
-                    (current_char=='X')?10:
-                    (current_char=='L')?50:
-                    (current_char=='C')?100:
-                    (current_char=='D')?500:
-                    (current_char=='M')?1000:
-                    0;
-            
-            if (digit<previous){
-                sum -= digit;
-            }
-            else{
-                sum += digit;
-            }
-            previous = digit;
+            num = (last>letterValue)?(num-letterValue):(num+letterValue);
+            last = letterValue;
+            i--;
         }
-        return(sum);
+        return num;
     }
 }
