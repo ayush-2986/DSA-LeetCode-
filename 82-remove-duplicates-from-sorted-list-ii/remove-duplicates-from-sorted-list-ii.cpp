e7e4b12572f0c4a432fe355c505ecae52c4ec0f3
ayush-2986 <2986.ayush@gmail.com>
2026-09-26 09:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (44.68%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -16,33 +16,36 @@ public:
         ListNode *result = nullptr;
         ListNode *tail = nullptr;
         ListNode *prev = nullptr;
+        int count = 1;
 
         while(temp){
-            ListNode *node = new ListNode;
+            // ListNode *node = new ListNode;
             if(!tail){
                 if ((temp->next && temp->val!=temp->next->val)|| !temp->next){
 
                     if ((prev && prev->val!=temp->val)|| !prev){
-                        node->val = temp->val;
-                        node->next = nullptr;
-                        result = node;
-                        tail = node;
+                        // node->val = temp->val;
+                        // node->next = nullptr;
+                        result = temp;
+                        tail = result;
                     }
                 }
             }
             else{
                 if((temp->next && temp->next->val!=temp->val)||(!temp->next)){
                     if(prev->val!=temp->val){
-                    node->val = temp->val;
-                    node->next = nullptr;
-                    tail->next = node;
-                    tail = node;
+                    // node->val = temp->val;
+                    // node->next = nullptr;
+                    tail->next = temp;
+                    tail = tail->next;
                 }
                 }
             }
             prev = temp;
             temp = temp->next;
+            count++;
         }
+        if (tail) tail->next = nullptr;
         return result;
         
     }
